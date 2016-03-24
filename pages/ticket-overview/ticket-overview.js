e67e5e9b08554c24b037824bd2461e1ca31ddb1a
Vincent <vincent.vandemeent@student.hu.nl>
2016-03-24 15:32:44 +0100

    ticket-overview almost done

--- pages/ticket-overview/ticket-overview.js
+++ pages/ticket-overview/ticket-overview.js
@@ -1,52 +1,103 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	/**
-	 * @ngdoc function
-	 * @name trunkApp.controller:TicketOverviewCtrl
-	 * @description
-	 * # TestCtrl
-	 * Controller of the trunkApp
-	 */
-	window.app.controller('TicketOverviewCtrl', ['$scope', function ($scope) {
-		
+    /**
+     * @ngdoc function
+     * @name trunkApp.controller:TicketOverviewCtrl
+     * @description
+     * # TestCtrl
+     * Controller of the trunkApp
+     */
+    window.app.controller('TicketOverviewCtrl', ['$scope', function ($scope) {
+
         $scope.priority = '';
-        $scope.creation = 'Newest';
+        $scope.creation = 'ticketnumber';
         $scope.type = '';
-        
-        
-        $scope.tickets = [{'ticketnumber':'1', 
-                           'subject':'HET DOET HET NIET!', 
-                           'created': '12-03-2015',
-                           'type': 'bug',
-                           'status': 'unsigned',
-                           'priority': 'high',
+
+
+        $scope.tickets = [{
+                'ticketnumber': '1',
+                'subject': 'HET DOET HET NIET!',
+                'created': '12-03-2015',
+                'type': 'bug',
+                'status': 'unsigned',
+                'priority': 'high',
                            },
-                          
-                         {'ticketnumber':'2', 
-                           'subject':'HET DOET HET WEL!', 
-                           'created': '12-03-2012',
-                           'type': 'bug',
-                           'status': 'unsigned',
-                           'priority': 'high',
+
+            {
+                'ticketnumber': '2',
+                'subject': 'HET DOET HET WEL!',
+                'created': '12-03-2012',
+                'type': 'bug',
+                'status': 'unsigned',
+                'priority': 'high',
                            },
-                         
-                         {'ticketnumber':'3', 
-                           'subject':'HET DOET HET MAAR!', 
-                           'created': '12-03-2011',
-                           'type': 'feature',
-                           'status': 'unsigned',
-                           'priority': 'average',
+
+            {
+                'ticketnumber': '3',
+                'subject': 'HET DOET HET MAAR!',
+                'created': '12-03-2011',
+                'type': 'feature',
+                'status': 'unsigned',
+                'priority': 'average',
                            }
-                         
-                         
-                         
+
+
+
                          ];
-        
-        $scope.editTicket = function(ticketnumber) {
+
+        $scope.editTicket = function (ticketnumber) {
             console.log(ticketnumber);
         }
-        
-        
+
+        $scope.setPriorityColour = function (priority) {
+            var colour = "";
+            if (priority == "high") {
+                colour = "danger";
+            } else if (priority == "average") {
+                colour = "warning";
+            } else {
+                colour = "success";
+            }
+            return colour;
+        }
+
+        $scope.filterTickets = function (ticket) {
+            var filterSearch = false;
+            var filterType = false;
+            var filterPriority = false;
+
+            if ($scope.search != undefined && $scope.search != "") {
+                if (ticket.subject.toLocaleLowerCase().indexOf($scope.search.toLowerCase()) > -1) {
+                    filterSearch = true;
+                } else if (ticket.ticketnumber.toLocaleLowerCase().indexOf($scope.search.toLowerCase()) > -1) {
+                    filterSearch = true;
+                }
+            } else {
+                filterSearch = true;
+            }
+
+            if ($scope.type != "") {
+                if ($scope.type.toLowerCase().indexOf(ticket.type.toLowerCase()) > -1) {
+                    filterType = true;
+                }
+            } else {
+                filterType = true;
+            }
+
+            if ($scope.priority != "") {
+                if ($scope.priority.toLowerCase().indexOf(ticket.priority.toLowerCase()) > -1) {
+                    filterPriority = true;
+                }
+            } else {
+                filterPriority = true;
+            }
+
+            if (filterSearch && filterType && filterPriority) {
+                return true;
+            } else {
+                return false;
+            }
+        }
 	 }]);
 })();
