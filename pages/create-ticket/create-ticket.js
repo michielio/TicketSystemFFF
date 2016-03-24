0456617b11b8822f9f83ec84ebee7dd5b330b50b
michiel Blaauw <michiel Blaauw>
2016-03-24 14:22:23 +0100

    Ik have added functions that create a new ticket object in the Create-Ticket map. It is not working properly just yet :)-

--- pages/create-ticket/create-ticket.js
+++ pages/create-ticket/create-ticket.js
@@ -1,14 +1,62 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	/**
-	 * @ngdoc function
-	 * @name trunkApp.controller:CreateTicketCtrl
-	 * @description
-	 * # TestCtrl
-	 * Controller of the trunkApp
-	 */
-	window.app.controller('CreateTicketCtrl', ['$scope', function ($scope) {
-		$scope.message = 'hello world';
+    /**
+     * @ngdoc function
+     * @name trunkApp.controller:CreateTicketCtrl
+     * @description
+     * # TestCtrl
+     * Controller of the trunkApp
+     */
+    window.app.controller('CreateTicketCtrl', ['$scope', function ($scope) {
+        $scope.message = 'hello world';
+        $scope.date = new Date();
+        
+        $scope.ticketTypes = [{
+                id: 1,
+                name: "feature"
+            },
+            {
+                id: 2,
+                name: "bug"
+            }, {
+                id: 3,
+                name: "unclear"
+            }, {
+                id:4,
+                name:"other"
+            }] ;
+        
+        $scope.createTicket = function(){
+            var newTicket = CreateNewTicketObject() ;
+            console.log(newTicket) ;
+        } 
 	 }]);
+    
+    function CreateNewTicketObject(){
+        var creationTime = new Date() ;
+        creationTime = creationTime.toString() ;
+        var unsolved = "unsolved" ;
+        var na = "NA" ;
+        
+        var newTicket = {
+            ticketnr : $scope.ticketNumber,
+            subject : $scope.ticketName,
+            creationTime : creationTime,
+            type : $scope.selectedTicketType,
+            email :$scope.emailConnectedToTicket,
+            description : $scope.ticketDescription,
+            status : unsolved,
+            priority : na,
+            recieve_updates : $scope.revieveUpdates,
+            solution : na,
+            solvedDate : na
+        }
+        
+        return newTicket ;
+    }
+    
+    function AddNewTicketToDb(newTicket){
+        
+    }
 })();
