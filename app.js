5cbc3fbf5e252ea8d2e624a2366db05006b55a6c
p4k03n4t0r <paul__94@hotmail.com>
2016-03-24 11:48:14 +0100

    Angular app werkend

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 'use strict';
 
-window.base_dir = '';
 /**
  * @ngdoc overview
- * @name trunkApp
+ * @name myApp
  * @description
- * # trunkApp
+ * # myApp
  *
  * Main module of the application.
  */
-window.app = angular.module('trunkApp', [
-	'ngAnimate',
-	'ngRoute'
-])
-.config(['$routeProvider', function ($routeProvider) {
-	$routeProvider
-		.when('/', {
-			templateUrl: window.base_dir + 'pages/test/test.html',
-			controller: 'TestCtrl',
-			controllerAs: 'ctrl'
-		})
-		.otherwise({
-			redirectTo: '/'
-		});
-}]);
+window.app = angular.module('myApp', []));
