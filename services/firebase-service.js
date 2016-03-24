1f85f01889dc370fe53444f8977ebb26a3618131
woute <wouter.fennis@student.hu.nl>
2016-03-24 13:58:09 +0100

    Added firebase service and dependencies

---
+++ services/firebase-service.js
@@ -0,0 +1,40 @@
+(function () {
+    'use strict';
+    window.app.service('FirebaseService', [function () {
+
+        var FIREBASE_DATABASE_URL = "https://glowing-inferno-1129.firebaseio.com/";
+        var ref = new Firebase(FIREBASE_DATABASE_URL);
+        var globalAuthData = undefined;
+
+        this.login = function (email, password) {
+            ref.authWithPassword({
+                email: user.email,
+                password: user.password
+            }, function (error, authData) {
+                if (error) {
+                    console.log("Login Failed!", error);
+                } else {
+                    globalAuthData = authData;
+                    console.log("Authenticated successfully with payload:", authData);
+                }
+            });
+        }
+
+        this.sessionExists = function () {
+            var isValidSession = false;
+            if (globalAuthData === undefined) {
+                isValidSession = true;
+            }
+            return isValidSession;
+        }
+
+        this.getGlobalAuthData() {
+            return globalAuthData;
+        }
+
+        this.setGlobalAuthData(newGlobalAuthData) {
+            globalAuthData = newGlobalAuthData;
+        }
+
+    }]);
+})();
