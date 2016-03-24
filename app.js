6066a3372eca88375185c583d14e76ed1c02d1f6
woute <wouter.fennis@student.hu.nl>
2016-03-24 12:43:09 +0100

    Further improvements on setup and added empty html pages. Also configured app.js so that navigation can work

--- app.js
+++ app.js
@@ -8,4 +8,41 @@
  *
  * Main module of the application.
  */
-window.app = angular.module('myApp', []);
+window.app = angular.module('myApp', ['ui.router'])
+
+
+
+.config(function ($stateProvider, $urlRouterProvider) {
+console.log("hallo!");
+    $stateProvider
+
+        .state('login', {
+        url: '/',
+        templateUrl: 'pages/login/login.html',
+        controller: 'LoginCtrl',
+        onEnter: function ($state) {
+            console.log("Welcome on login");
+        }
+    })
+
+    .state('create-ticket', {
+        url: '/create-ticket',
+        templateUrl: 'pages/create-ticket/create-ticket.html',
+        controller: 'CreateTicketCtrl',
+        onEnter: function ($state) {
+            console.log("Welcome on create-ticket");
+        }
+    })
+    
+    .state('ticket-overview', {
+        url: '/ticket-overview',
+        templateUrl: 'pages/overview-ticket/overview-ticket.html',
+        controller: 'TicketOverviewCtrl',
+        onEnter: function ($state) {
+            console.log("Welcome on ticket-overview");
+        }
+    })
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/');
+});
